9d317cc0254d8624d0840d3ae642a0c28ff0756e
iamrpean <iamrpean@noreply.github.com>
2021-07-07 01:33:15 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/jpg" href="assets/img/favicon.ico" />
+    <link rel="stylesheet" href="assets/css/style2.css">
+    <style data-react-helmet="true" type="text/css">
+        html.has-scroll-smooth {
+            overflow: hidden !important;
+        }
+
+        html.has-scroll-dragging {
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        .has-scroll-smooth body {
+            overflow: hidden;
+        }
+
+        .has-scroll-smooth [data-scroll-container] {
+            min-height: 100vh;
+        }
+
+        .c-scrollbar {
+            position: absolute;
+            right: 0;
+            top: 0;
+            width: 11px;
+            height: 100vh;
+            transform-origin: center right;
+            transition: transform 0.3s, opacity 0.3s;
+            opacity: 0;
+            z-index: 99999;
+        }
+
+        .c-scrollbar:hover {
+            transform: scaleX(1.45);
+        }
+
+        .c-scrollbar:hover,
+        .has-scroll-dragging .c-scrollbar,
+        .has-scroll-scrolling .c-scrollbar {
+            opacity: 1;
+        }
+
+        .c-scrollbar_thumb {
+            position: absolute;
+            top: 0;
+            right: 0;
+            background-color: #bc8441;
+            opacity: 0.5;
+            width: 7px;
+            border-radius: 10px;
+            margin: 2px;
+            cursor: -webkit-grab;
+            cursor: grab;
+        }
+
+        .has-scroll-dragging .c-scrollbar_thumb {
+            cursor: -webkit-grabbing;
+            cursor: grabbing;
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>WEDDING</title>
+</head>
+
+<body>
+
+    <div id="container">
+        <div id="container">
+            <main>
+                <div class="scroll-container">
+                    <div class="mobile-menu show">
+                        <div class="navigation expanded">
+                            <div class="menu-items expanded">
+                                <span class="item " href="#when">When &amp; Where</span>
+                                <span class="item " href="#story">Our Story</span>
+                                <span class="item " href="https://michelletonye.app.rsvpify.com/" target="_blank">Livestream</span>
+                                <span class="item ">Gallery</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="navigation-container">
+                        <div class="initials menu-open">
+                            <span class="piece">T</span>
+                            <span class="piece">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" style="transform: rotate(360deg);">
+                                    <path d="M12 20a1 1 0 0 1-.561-.172c-.225-.151-5.508-3.73-7.146-5.371C2.462 12.626 2.25 10.68 2.25 9.375A5.38 5.38 0 0 1 7.625 4c1.802 0 3.398.891 4.375 2.256A5.373 5.373 0 0 1 16.375 4a5.38 5.38 0 0 1 5.375 5.375c0 1.305-.212 3.251-2.043 5.082c-1.641 1.641-6.923 5.22-7.146 5.371A1 1 0 0 1 12 20zM7.625 6A3.379 3.379 0 0 0 4.25 9.375c0 1.093.173 2.384 1.457 3.668c1.212 1.212 4.883 3.775 6.293 4.746c1.41-.971 5.081-3.534 6.293-4.746c1.284-1.284 1.457-2.575 1.457-3.668C19.75 7.514 18.236 6 16.375 6S13 7.514 13 9.375a1 1 0 1 1-2 0A3.379 3.379 0 0 0 7.625 6z" fill="#e6d9b2"></path>
+                                </svg>
+                            </span>
+                            <span class="piece">E</span>
+                        </div>
+                        <div class="date" style="height:120px;">
+                            <style>
+                                .scroll-container .navigation-container .date:after {
+                                    bottom: -60.5vh
+                                }
+                            </style>
+                            <span class="piece">00</span>
+                            <span class="piece">/</span>
+                            <span class="piece">08</span>
+                            <span class="piece">/</span>
+                            <span class="piece">21</span>
+                        </div>
+                    </div>
+
+                    <section class="home" id="home">
+                        <div class="gatsby-image-wrapper" style="
+                        position: absolute; overflow: hidden; display: inline-block; width: 100%; height: 113vh; top: 0px; left: 0px; margin-top: -10vh; opacity: 0.5; pointer-events: none;">
+                            <picture>
+                                <img src="assets/img/pexels-emma-bauso-2253870.jpg" alt="photo1cropped.jpg" width="250" height="612" loading="eager" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 1; transition: none 0s ease 0s;">
+                            </picture>
+                        </div>
+                        <div class="home-inner" id="home">
+                            <div class="is-inview" style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -130.8, 0, 1);">
+                                <div class="wow is-inview" style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -47.7803, 0, 1);">
+                                    <div style="transform-style: preserve-3d; transition: transform 0.3s ease-in-out 0s; position: relative; will-change: transform;">
+                                        <div class="us">
+                                            <span class="piece">Tami</span>
+                                            <span class="piece">&amp;</span>
+                                            <span class="piece">Elsa</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section class="when" id="when">
+                        <div class="box-dad">
+                            <div class="box is-inview" style="box-shadow: rgb(26, 22, 13) 0px 0px 60px; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -16.8, 0, 1);">
+                                <span class="month is-inview" data-scroll="true" data-scroll-speed="5" data-scroll-target="#when" style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -14, 0, 1);">Agustus</span>
+                                <div class="who is-inview" data-scroll="true" data-scroll-speed="2" data-scroll-target="#when" style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -5.6, 0, 1);">
+                                    <p class="larger">19</p>
+                                    <div style="animation: 1.4s cubic-bezier(0.02, 0.13, 0, 0.99) 1s 1 normal none running fade-in-top;"><span class="sc-bdfBwQ cIKpxU"><span class="sc-gsTCUz bhdLno" style="top: 17%; left: 32%;"><svg width="10" height="10" viewBox="0 0 68 68" fill="none" class="sc-dlfnbm bcaJjD">
+                                                    <path d="M26.5 25.5C19.0043 33.3697 0 34 0 34C0 34 19.1013 35.3684 26.5 43.5C33.234 50.901 34 68 34 68C34 68 36.9884 50.7065 44.5 43.5C51.6431 36.647 68 34 68 34C68 34 51.6947 32.0939 44.5 25.5C36.5605 18.2235 34 0 34 0C34 0 33.6591 17.9837 26.5 25.5Z" fill="#f0d37de3"></path>
+                                                </svg></span><span class="sc-gsTCUz bhdLno" style="top: 12%; left: 17%;"><svg width="12" height="12" viewBox="0 0 68 68" fill="none" class="sc-dlfnbm bcaJjD">
+                                                    <path d="M26.5 25.5C19.0043 33.3697 0 34 0 34C0 34 19.1013 35.3684 26.5 43.5C33.234 50.901 34 68 34 68C34 68 36.9884 50.7065 44.5 43.5C51.6431 36.647 68 34 68 34C68 34 51.6947 32.0939 44.5 25.5C36.5605 18.2235 34 0 34 0C34 0 33.6591 17.9837 26.5 25.5Z" fill="#f0d37de3"></path>
+                                                </svg></span><strong class="sc-hKgILt gTLZXx">
+                                                <div class="circle is-inview" data-scroll="true" data-scroll-speed="7" data-scroll-target="#when" style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 148.4, 0, 1);">
+                                                    <div class=" gatsby-image-wrapper" style="position: relative; overflow: hidden; width: 100%; height: auto;">
+                                                        <div aria-hidden="true" style="width: 100%; padding-bottom: 69.4118%;"></div>
+                                                        <picture>
+                                                            <source type="image/webp" srcset="/static/6d186bfc2165e731c4e477d74642e207/22d41/location-wedding.webp 85w,/static/6d186bfc2165e731c4e477d74642e207/95e6e/location-wedding.webp 170w,/static/6d186bfc2165e731c4e477d74642e207/0817f/location-wedding.webp 340w,/static/6d186bfc2165e731c4e477d74642e207/20745/location-wedding.webp 510w,/static/6d186bfc2165e731c4e477d74642e207/32a62/location-wedding.webp 528w" sizes="(max-width: 340px) 100vw, 340px">
+                                                            <source srcset="/static/6d186bfc2165e731c4e477d74642e207/e98de/location-wedding.jpg 85w,/static/6d186bfc2165e731c4e477d74642e207/06e87/location-wedding.jpg 170w,/static/6d186bfc2165e731c4e477d74642e207/bb94b/location-wedding.jpg 340w,/static/6d186bfc2165e731c4e477d74642e207/78264/location-wedding.jpg 510w,/static/6d186bfc2165e731c4e477d74642e207/10683/location-wedding.jpg 528w" sizes="(max-width: 340px) 100vw, 340px"><img sizes="(max-width: 340px) 100vw, 340px" srcset="/static/6d186bfc2165e731c4e477d74642e207/e98de/location-wedding.jpg 85w,/static/6d186bfc2165e731c4e477d74642e207/06e87/location-wedding.jpg 170w,/static/6d186bfc2165e731c4e477d74642e207/bb94b/location-wedding.jpg 340w,/static/6d186bfc2165e731c4e477d74642e207/78264/location-wedding.jpg 510w,/static/6d186bfc2165e731c4e477d74642e207/10683/location-wedding.jpg 528w" src="/static/6d186bfc2165e731c4e477d74642e207/bb94b/location-wedding.jpg" alt="location-wedding.jpg" loading="lazy" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 0; transition: opacity 500ms ease 0s;">
+                                                        </picture>
+                                                        <noscript>
+                                                            <picture>
+                                                                <source type='image/webp' srcset="/static/6d186bfc2165e731c4e477d74642e207/22d41/location-wedding.webp 85w,/static/6d186bfc2165e731c4e477d74642e207/95e6e/location-wedding.webp 170w,/static/6d186bfc2165e731c4e477d74642e207/0817f/location-wedding.webp 340w,/static/6d186bfc2165e731c4e477d74642e207/20745/location-wedding.webp 510w,/static/6d186bfc2165e731c4e477d74642e207/32a62/location-wedding.webp 528w" sizes="(max-width: 340px) 100vw, 340px" />
+                                                                <source srcset="/static/6d186bfc2165e731c4e477d74642e207/e98de/location-wedding.jpg 85w,/static/6d186bfc2165e731c4e477d74642e207/06e87/location-wedding.jpg 170w,/static/6d186bfc2165e731c4e477d74642e207/bb94b/location-wedding.jpg 340w,/static/6d186bfc2165e731c4e477d74642e207/78264/location-wedding.jpg 510w,/static/6d186bfc2165e731c4e477d74642e207/10683/location-wedding.jpg 528w" sizes="(max-width: 340px) 100vw, 340px" /><img loading="lazy" sizes="(max-width: 340px) 100vw, 340px" srcset="/static/6d186bfc2165e731c4e477d74642e207/e98de/location-wedding.jpg 85w,/static/6d186bfc2165e731c4e477d74642e207/06e87/location-wedding.jpg 170w,/static/6d186bfc2165e731c4e477d74642e207/bb94b/location-wedding.jpg 340w,/static/6d186bfc2165e731c4e477d74642e207/78264/location-wedding.jpg 510w,/static/6d186bfc2165e731c4e477d74642e207/10683/location-wedding.jpg 528w" src="/static/6d186bfc2165e731c4e477d74642e207/bb94b/location-wedding.jpg" alt="location-wedding.jpg" style="position:absolute;top:0;left:0;opacity:1;width:100%;height:100%;object-fit:cover;object-position:center" />
+                                                            </picture>
+                                                        </noscript>
+                                                    </div>
+                                                </div>
+                                            </strong></span>
+                                    </div>
+                                    <div class="sans-caps is-inview" data-scroll="true" data-scroll-speed="3" data-scroll-target="#when" style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -8.4, 0, 1);">
+                                        Michelle and Tonye had a small wedding celebration! We said "I do", in the eyes of God, family and friends (via livestream) here in Vancouver, BC.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
